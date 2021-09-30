569399dbe97628704b446bb84aa27c2fbe67db6a
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2021-09-30 09:27:32 +0200

    Update ex2_intensite_lumineuse.py

--- 02 La carte MicroBit/exercices/ex2_intensite_lumineuse.py
+++ 02 La carte MicroBit/exercices/ex2_intensite_lumineuse.py
@@ -5,7 +5,7 @@ intensite = 9
 while True:
     display.set_pixel(2, 2, intensite)
     if button_a.is_pressed() and intensite >0:
-        intensite -= 1
+        intensite = intensite - 1
     if button_b.is_pressed() and intensite <9:
-        intensite += 1
+        intensite = intensite + 1
     sleep(100)
