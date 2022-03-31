017ff6f8f0b03399e3e821b5921fb5d6f2a97c2e
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2022-03-31 09:56:09 +0200

    Add files via upload

---
+++ 12 Dictionnaire/exercices/ex2.py
@@ -0,0 +1,17 @@
+personnes = {
+    "Jean Bon": 12,
+    "César Bistrukla": 10,
+    "Marc Assin": 11,
+    "John Doeuf": 10,
+    "Agathe Leblouze": 11,
+    "Guy Dondvelo": 12,
+    "Eva Nouissement": 12,
+    "Juda Nana": 10,
+    "Laurie Culayr": 11,
+    "Mélanie Chedanslejardin": 10,
+    "Alain Disoir": 10,
+    "Jean Foupasune": 10,
+    "Sophie Fonsec": 10,
+    "Jean Filmongilet": 11,
+    "Gédéon Groisdansmabaignoire": 12
+    }
