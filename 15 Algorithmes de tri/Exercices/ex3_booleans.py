803af34f34c2bec4e4dcebaa11e56b2d10ee3dfe
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2022-05-10 10:22:23 +0200

    Update ex3_booleans.py

--- 15 Algorithmes de tri/Exercices/ex3_booleans.py
+++ 15 Algorithmes de tri/Exercices/ex3_booleans.py
@@ -2,25 +2,42 @@
 
 import random as rd
 
-#taille du tableau
-n=10
 
-#un tableau de boolean
-T=[bool(rd.randint (0 ,1)) for i in range(n)]
-print(T)
+def tri_bool(tab):
+    # on compte le nombre de nb de False
+    nb_false = 0
+    for i in range(len(tab)):
+        if tab[i]==False:
+            nb_false = nb_false + 1
+    # on reconstruit le tableau
+    for i in range(0,nb_false):
+        tab[i] = False
+    for i in range(nb_false,n):
+        tab[i] = True
+    return tab
 
-#on compte le nombre de False
-nb_False=0
-for i in range(0,n) :
-    if T[i]==False:
-        nb_False += 1
+# une variante avec la reconstruction par compréhension
+def tri_bool_v2(tab):
+    # on compte le nombre de nb de False
+    nb_false = 0
+    for i in range(len(tab)):
+        if tab[i]==False:
+            nb_false = nb_false + 1
+    # on reconstruit le tableau
+    tab = [i>=nb_false for i in range(n)]
+    return tab
 
-#on remplit le tableau de False puis de True
-for i in range(0,nb_False) :
-    T[i] = False
-for i in range(nb_False, n):
-    T[i] = True
 
-#petite vérification
-print(T)
-
+####
+# pour vérification
+####
+if __name__ == "__main__":
+    #   taille du tableau
+    n=10
+    # un tableau de boolean
+    T=[bool(rd.randint (0 ,1)) for i in range(n)]
+    print(T)
+    #T = tri_bool(T)
+    #print(T)
+    T = tri_bool_v2(T)
+    print(T)
