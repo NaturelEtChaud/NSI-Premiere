a6d2a5118ce53527861fb91b004856d52b8a1a0a
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2022-05-19 08:21:23 +0200

    Add files via upload

---
+++ 16 Algorithmes gloutons/sac_a_dos pour commencer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+obj =[{"objet": "A", "masse" : 13, "valeur" : 700},
+    {"objet": "B", "masse" : 12, "valeur" : 50},
+    {"objet": "C", "masse" : 8, "valeur" : 200},
+    {"objet": "D", "masse" : 10, "valeur" : 300},
+    {"objet": "E", "masse" : 14, "valeur" : 600},
+    {"objet": "F", "masse" : 18, "valeur" : 800}]
+
+
