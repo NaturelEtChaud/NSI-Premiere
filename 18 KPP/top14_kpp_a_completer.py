fcc1ad95c01951a87393ba2001dbc225fefc5157
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2022-05-31 07:58:27 +0200

    Add files via upload

---
+++ 18 KPP/top14_kpp_a_completer.py
@@ -0,0 +1,28 @@
+import codecs #pour lire en UTF8, n'est pas toujours nécessaire
+import csv
+
+joueurs = []
+
+with codecs.open('top14.csv', 'r', 'utf-8') as fichier:
+    csvtop14 = csv.DictReader(fichier, delimiter=',')
+    for ligne in csvtop14:
+        joueurs.append(dict(ligne))
+
+# pour vérification on affiche le premier joueur
+print(joueurs[2])
+
+# fonction distance
+def dist(A,B):
+    '''
+    A[0] est l'abscisse du point
+    A[1] est l'ordonnée du point
+    même chose pour le point B
+    '''
+    pass
+
+# fonction pour déterminer les k plus proches voisins
+def kpp(poids, taille, k):
+    '''
+    renvoie la liste des postes des k plus proches voisins de l'individu ayant le poids et la taille donnés en argument
+    '''
+    pass
